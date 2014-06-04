cd72b5028605c374c10c5271b356df04f6ab2888
moehlc <cmohl@yahoo.com>
2014-06-04 14:27:11 +0200

    added averageAndProject_gauss.py for filter size comparison

---
+++ averageAndProject_gauss.py
@@ -0,0 +1,93 @@
+import sys
+sys.path.append("/mnt/moehlc/home/idaf_library")
+import libidaf.idafIO as io
+import numpy as np
+from scipy.stats import nanmean
+import pickle
+import os
+
+import multiprocessing as mp
+
+
+def exportPickle(vol,savepath, fname):
+	
+	if fname.find('.') != -1: #if file extension zB .tif present
+		fnameTrunc = fname[0:fname.find('.')]
+	else:
+		fnameTrunc = fname		
+
+
+	try:
+		os.makedirs(savepath)
+	except:
+		print(savepath+' already exists')
+
+	pickle.dump(vol,open(savepath + fnameTrunc +'.pickle','w'))
+
+
+def loadAndAverage(path,pattern,shape):
+	vol = np.zeros(shape) # 3d volume for summing up
+	nn = np.zeros(shape) # 3 D matrix for number of samples
+	fnames = io.getFilelistFromDir(path,pattern) #list of tiff stacks to be filtered
+	n = len(fnames)
+
+	#import each volume, sum pixels and keep track of sample number
+	for i in range(n):
+		volTmp = pickle.load(open(path + fnames[i],'rb'))
+		nn [~np.isnan(volTmp)] += 1 # increase number of samples by 1 where volTmp is not nan
+		volTmp[np.isnan(volTmp)] = 0 # set nans to zero
+		vol += volTmp
+		print(str(100*i/float(n)))
+	vol_average = vol/nn
+	
+	return vol_average, nn	
+
+
+def loadAverageSave(path,pattern,shape,savepath,savename):
+	vol_average, nn = loadAndAverage(path, pattern, shape)
+	exportPickle(vol_average,savepath,'volAverage_' + savename)
+	exportPickle(nn,savepath,'volNN_' + savename)
+
+def loadAverageSave_parallel(path,pattern,shape,savepath,savename):
+	mp.Process(target = loadAverageSave, args = (path,pattern,shape,savepath,savename)).start() #parallel processing	
+
+
+path_ad = '/mnt/moehlc/idaf/IDAF_Projects/140327_raman_bloodvessel_mri/data/filteredPickles/angio_ad/'
+path_wt = '/mnt/moehlc/idaf/IDAF_Projects/140327_raman_bloodvessel_mri/data/filteredPickles/angio_wt/'
+
+
+
+
+pattern1_20 = 'filtered_Size_20distanceSkel' # gaussfiltered distance images
+pattern2_20 = 'filtered_Size_20distanceSkel'
+pattern1_10 = 'filtered_Size_10distanceSkel' # gaussfiltered distance images
+pattern2_10 = 'filtered_Size_10distanceSkel'
+
+savepath = '/mnt/moehlc/idaf/IDAF_Projects/140327_raman_bloodvessel_mri/data/averagedPickles/'
+
+savename1_20_wt = 'distance_Size_20_angioWT'
+savename2_20_wt = 'flow_Size_20_angioWT'
+savename1_10_wt = 'distance_Size_10_angioWT'
+savename2_10_wt = 'flow_Size_10_angioWT'
+
+savename1_20_ad = 'distance_Size_20_angioAD'
+savename2_20_ad = 'flow_Size_20_angioAD'
+savename1_10_ad = 'distance_Size_10_angioAD'
+savename2_10_ad = 'flow_Size_10_angioAD'
+
+
+
+shape = (320,320,272) 
+
+loadAverageSave_parallel(path_ad,pattern1_20,savepath,savename1_20_ad)
+loadAverageSave_parallel(path_ad,pattern2_20,savepath,savename2_20_ad)
+#loadAverageSave_parallel(path_ad,pattern1_10,savepath,savename1_10_ad)
+#loadAverageSave_parallel(path_ad,pattern2_10,savepath,savename2_10_ad)
+
+
+#loadAverageSave_parallel(path_wt,pattern1_20,savepath,savename1_20_wt)
+#loadAverageSave_parallel(path_wt,pattern2_20,savepath,savename2_20_wt)
+#loadAverageSave_parallel(path_wt,pattern1_10,savepath,savename1_10_wt)
+#loadAverageSave_parallel(path_wt,pattern2_10,savepath,savename2_10_wt)
+
+
---
+++ parallel_nanmeanGaussWeightedFilterOptimizedSize10_angioAD.py
@@ -0,0 +1,114 @@
+import sys
+sys.path.append("/mnt/moehlc/home/idaf_library")
+#import mahotas
+import vigra
+import libidaf.idafIO as io
+import numpy as np
+from scipy import ndimage
+from scipy.stats import nanmean
+#import matplotlib.pyplot as plt
+import time
+import pickle
+import os
+import multiprocessing as mp
+
+
+def gaussWeight(dat,sigma,mu):
+	return 1./np.sqrt(2*np.pi*np.square(sigma))*np.exp(-np.square(dat-mu)/(2*np.square(sigma)))
+
+def streaming3Dfilter(data,outdata,sigma):
+	fsize = int(np.round(sigma*3)) # filter size
+	amax=np.array(data.shape)-1 #max index
+	amin=amax-amax #min index
+
+	xyz = np.array(data.nonzero())#coordinates
+	x = xyz[0,:]
+	y = xyz[1,:]
+	z = xyz[2,:]
+	datxyz = np.array(data[x,y,z])
+
+	for i in range(amax[0]+1): #x dim
+		for j in range(amax[1]+1): # y dim
+			for k in range(amax[2]+1): # z dim
+				
+				dist = np.sqrt(np.square(i-x) + np.square(j-y) + np.square(k-z)) 
+				ind = dist<= fsize 
+				weight = gaussWeight(dist[ind],sigma,0)
+				datsel = datxyz[ind]
+				if datsel.size == 0:
+					outdata[i,j,k] = np.nan
+				else:
+					outdata[i,j,k] = np.average(datsel,weights = weight)
+		print('writing slice ' + str(i) + 'to '+ outdata.filename)	
+		print('progress: ' + str(i/float(amax[0])*100) + ' percent done')
+		outdata.flush() #write to disk
+
+
+
+def importStack(path,fname,tmpStackDir):
+	absname = path +fname
+	zsize = vigra.impex.numberImages(absname)
+	im =vigra.readImage(absname, index = 0, dtype='FLOAT')
+	#vol = np.zeros([im.height,im.width,zsize])
+	
+	try:
+		os.makedirs(tmpStackDir)
+	except:
+		print(tmpStackDir+' already exists')
+
+	vol = np.memmap(tmpStackDir + fname[0:-4],dtype='float64',mode = 'w+', shape = (im.height,im.width,zsize))
+	#raise('hallo')
+	for i in range(zsize):
+		print("importing slice " + str(i) + ' of file '+fname)
+		im=np.squeeze(vigra.readImage(absname, index = i, dtype='FLOAT'))
+		vol[:,:,i] = im
+	vol.flush()
+	return vol
+
+def filterAndSave(fname,path,savepath,filterSize,volpath):
+	vol = importStack(path,fname,volpath)
+	
+	try:
+		os.makedirs(savepath)
+	except:
+		print(savepath+' already exists')
+	res = np.memmap(savepath + 'filtered_Size_'+ str(filterSize) + fname,dtype = 'float64', mode = 'w+', shape = vol.shape)	
+	streaming3Dfilter(vol, res,filterSize)
+	
+
+def filterAndSave_batch(pattern,path,savepath,filterSize,volpath):
+	fnames = io.getFilelistFromDir(path,pattern) #list of tiff stacks to be filtered
+	for i in range(len(fnames)):
+	#for i in range(1):
+		print('start filter process for '+fnames[i])
+		mp.Process(target = filterAndSave, args = (fnames[i],path,savepath,filterSize,volpath)).start() #parallel processing
+
+def filterAndSave_batch_serial(pattern,path,savepath,filterSize,volpath):
+	fnames = io.getFilelistFromDir(path,pattern) #list of tiff stacks to be filtered
+	for i in range(len(fnames)):
+	#for i in range(1):
+		print('start filter process for '+fnames[i])
+		filterAndSave(fnames[i],path,savepath,filterSize,volpath) #parallel processing
+
+
+
+if __name__ == '__main__':
+	
+	path = '/mnt/moehlc/idaf/IDAF_Projects/140327_raman_bloodvessel_mri/data/segmented/angio_ad/'
+	savepath = '/home/moehlc/raman_bloodvessel_dat/filteredVoldDat1/angio_ad/'
+	volpath = '/home/moehlc/raman_bloodvessel_dat/rawVoldat2/angio_ad/'
+
+
+	filterSize = 10
+
+
+	filterAndSave_batch('flowSkel',path,savepath,filterSize,volpath)
+	filterAndSave_batch('distanceSkel',path,savepath,filterSize,volpath)
+	#filterAndSave_batch_serial('flowSkel',path,savepath,filterSize)
+	#filterAndSave_batch('distanceSkel',path,savepath,filterSize)
+
+
+
+
+
+
